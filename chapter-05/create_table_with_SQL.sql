cac90fbfe573ca75b2cdae35d240ffe186c44937
Piethein Strengholt <piethein@strengholt-online.nl>
2025-01-20 10:55:48 +0100

    Update create_table_with_SQL.sql

--- chapter-05/create_table_with_SQL.sql
+++ chapter-05/create_table_with_SQL.sql
@@ -1,7 +1,7 @@
-CREATE TABLE customer (  
-    CustomerID INT,  
-    NameStyle BOOLEAN,  
-    Title STRING,  
-    FirstName STRING  
-)  
-USING delta  
+CREATE TABLE customer (
+    CustomerID INT COMMENT 'Customer identifier',
+    NameStyle BOOLEAN COMMENT 'Style of the name',
+    Title STRING COMMENT 'Title of the customer',
+    FirstName STRING COMMENT 'First name of the customer'
+)
+USING delta
